1db729275f645eeed22c98ad087d98f7379cff2a
patrickm53 <patrickmcenaney53@gmail.com>
2023-06-05 17:37:40 +0300

    navbar finish

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,6 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,70 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import classes from "./navbar.module.css";
+import person from "../../../public/person.jpg";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Navbar = () => {
+  const loggenIn = true;
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <div className={classes.container}>
-      <div className={classes.wrapper}>
-        <h2 className={classes.left}>
-          <Link href="/">BlogApp</Link>
-        </h2>
-        <ul className={classes.right}>
-          <>
-            <button className={classes.login}>Log in</button>
-            <Link href="/register">Register</Link>
-          </>
-        </ul>
+    <>
+      <div className={classes.container}>
+        <div className={classes.wrapper}>
+          <h2 className={classes.left}>
+            <Link href="/">BookApp</Link>
+          </h2>
+          <div className={classes.middle}>
+            <button>
+              <Link href="/">Ana Sayfa</Link>
+            </button>
+            <button>
+              <Link href="/populer">Populer</Link>
+            </button>
+            <button>
+              <Link href="/rastgele">Rastgele</Link>
+            </button>
+            <div className={classes.search}>
+              <input
+                type="text"
+                placeholder="Kitap Ara..."
+                value={searchTerm}
+                onChange={handleInputChange}
+              />
+              <AiOutlineSearch className={classes.icons} />
+            </div>
+          </div>
+          {loggenIn ? (
+            <button className={classes.navbarProfile}>
+              <Link href="/profile">
+                <Image
+                  alt="profilResmi"
+                  src={person}
+                  width="45"
+                  height="45"
+                  className={classes.image}
+                />
+              </Link>
+            </button>
+          ) : (
+            <ul className={classes.right}>
+              <>
+                <button className={classes.login}>Log in</button>
+                <Link href="/register">Register</Link>
+              </>
+            </ul>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
