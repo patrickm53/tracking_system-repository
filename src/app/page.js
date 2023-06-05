987bfdeac0a807ca74d4c5a2bf674f98e8040a02
patrickm53 <patrickmcenaney53@gmail.com>
2023-06-05 19:10:15 +0300

    product card started

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
+import ProductCard from "@/components/productCard/ProductCard";
 import React from "react";
+import classes from "./page.module.css";
 
 const Home = () => {
-  return <div>Projeye başlanılıcak</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <ProductCard />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/productCard/ProductCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductCard = () => {
+  return <div>deneme</div>;
+};
+
+export default ProductCard;
