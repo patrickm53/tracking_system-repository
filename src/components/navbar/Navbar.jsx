001a86f29c45f429df77a40cc99ca08b4d37f050
patrickm53 <patrickmcenaney53@gmail.com>
2023-06-06 17:52:25 +0300

    logo add

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import classes from "./navbar.module.css";
 import person from "../../../public/person.jpg";
+import logo from "../../../public/logo.png";
 import { AiOutlineSearch } from "react-icons/ai";
 
 const Navbar = () => {
@@ -20,7 +21,9 @@ const Navbar = () => {
       <div className={classes.container}>
         <div className={classes.wrapper}>
           <h2 className={classes.left}>
-            <Link href="/">BookApp</Link>
+            <Link href="/">
+              <Image alt="logo" src={logo} height="50" className="logoImage" />
+            </Link>
           </h2>
           <div className={classes.middle}>
             <button>
