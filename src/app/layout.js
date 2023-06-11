f3c74d9234f3c1c66fb4e868bb8faff7319f33ce
patrickm53 <patrickmcenaney53@gmail.com>
2023-06-11 22:24:49 +0300

    footer add

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children, session }) {
         <SessionProvider session={session}>
           <Navbar />
           {children}
+          <Footer />
         </SessionProvider>
       </body>
     </html>
