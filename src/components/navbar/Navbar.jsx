2fe1dc7a525f25ccb93a9215d7089aba46a79a94
patrickm53 <patrickmcenaney53@gmail.com>
2023-08-18 12:27:37 +0300

    navbar hover effect add

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,15 @@ import React, { useState } from "react";
 import classes from "./navbar.module.css";
 import person from "../../../public/person.jpg";
 import logo from "../../../public/logo.png";
-import { AiOutlineSearch, AiOutlinePlusCircle } from "react-icons/ai";
+import {
+  AiOutlineSearch,
+  AiOutlinePlusCircle,
+  AiFillHome,
+} from "react-icons/ai";
+import { ImBooks } from "react-icons/im";
+import { GiPerspectiveDiceSixFacesRandom } from "react-icons/gi";
+import { IoMdSettings } from "react-icons/io";
+import { HiUserGroup } from "react-icons/hi";
 import { signIn, useSession } from "next-auth/react";
 import Search from "../search/Search";
 
@@ -24,13 +32,28 @@ const Navbar = () => {
           </h2>
           <div className={classes.middle}>
             <button>
-              <Link href="/">Ana Sayfa</Link>
+              <AiFillHome />
+              <Link className={classes.navbarText} href="/">
+                Ana Sayfa
+              </Link>
             </button>
             <button>
-              <Link href="/kitaplar">Tüm Kitaplar</Link>
+              <ImBooks />
+              <Link className={classes.navbarText} href="/kitaplar">
+                Tüm Kitaplar
+              </Link>
             </button>
             <button>
-              <Link href="/rastgele">Rastgele</Link>
+              <GiPerspectiveDiceSixFacesRandom />
+              <Link className={classes.navbarText} href="/rastgele">
+                Rastgele
+              </Link>
+            </button>
+            <button>
+              <HiUserGroup />
+              <Link className={classes.navbarText} href="/community">
+                Topluluk
+              </Link>
             </button>
             <Search />
           </div>
@@ -50,6 +73,11 @@ const Navbar = () => {
                     height="45"
                     className={classes.image}
                   />
+                </Link>
+              </button>
+              <button>
+                <Link href={"/settings"}>
+                  <IoMdSettings className={classes.settingsIcon} />
                 </Link>
               </button>
             </div>
