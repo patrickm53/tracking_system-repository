2df376d91fbb8b23f5ceddb0c2b76df226f4eee9
patrickm53 <patrickmcenaney53@gmail.com>
2023-09-10 00:02:15 +0300

    settingProfile page continue

--- src/components/settings-profile/SettingsProfile.jsx
+++ src/components/settings-profile/SettingsProfile.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from "next/image";
+import { background } from "../../../public/background2.jpg";
+import classes from "./settingsProfile.module.css";
 
 const SettingsProfile = ({ user }) => {
-  return <div>{user.name}</div>;
+  return (
+    <div>
+      <div>
+        <Image
+          className={classes.backgroundImage}
+          alt="background"
+          src={background}
+          heigh={100}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SettingsProfile;
