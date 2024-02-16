b4756804997afff06ff2297d58505e37b10bc7f2
patrickm53 <patrickmcenaney53@gmail.com>
2024-02-16 18:08:40 +0300

    build error v1.6

--- src/app/api/deneme/route.js
+++ src/app/api/deneme/route.js
@@ -1,6 +1,7 @@
 export async function GET(req) {
+  const deneme = process.env.DENEME_TEXT;
   try {
-    return new Response("succces", { status: 200 });
+    return new Response(deneme, { status: 200 });
   } catch (error) {
     return new Response("basarısız", { status: 500 });
   }
